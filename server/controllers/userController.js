f994430e2522b95302ef828445d6f9e0860d19d9
mannco1 <f.mannco@yandex.ru>
2024-04-29 20:05:21 +0300

    second

---
+++ server/controllers/userController.js
@@ -0,0 +1,28 @@
+const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
+
+class UserController {
+    async registration(req, res, next) {
+       
+    }
+
+    async login(req, res, next) {
+      
+    }
+
+    async check(req, res, next) {
+       
+    }
+}
+
+module.exports = new UserController()
