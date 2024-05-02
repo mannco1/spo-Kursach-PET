11e839a6cc5fc0e82654294e7875a6296825d124
mannco1 <f.mannco@yandex.ru>
2024-05-02 22:10:44 +0300

    Update models.js

--- server/models/models.js
+++ server/models/models.js
@@ -1,80 +1,34 @@
-// Импортируем Sequelize
+
 const { Sequelize, DataTypes } = require('sequelize');
 
-
-
-// Определяем модель Музыкантов
 const Musician = sequelize.define('Musician', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  instrument: {
-    type: DataTypes.STRING
-  }
+  name: { type: DataTypes.STRING, allowNull: false },
+  instrument: { type: DataTypes.STRING }
 });
 
-// Определяем модель Ансамблей
 const Ensemble = sequelize.define('Ensemble', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  type: {
-    type: DataTypes.STRING
-  }
+  name: { type: DataTypes.STRING, allowNull: false },
+  type: { type: DataTypes.STRING }
 });
 
-// Определяем модель Произведений
+
 const Composition = sequelize.define('Composition', {
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  composer: {
-    type: DataTypes.STRING
-  }
+  title: { type: DataTypes.STRING, allowNull: false },
+  composer: { type: DataTypes.STRING }
 });
 
-// Определяем модель Пластинок
+
 const Record = sequelize.define('Record', {
-  stickerNumber: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    unique: true
-  },
-  releaseDate: {
-    type: DataTypes.DATE
-  },
-  wholesalePrice: {
-    type: DataTypes.FLOAT
-  },
-  retailPrice: {
-    type: DataTypes.FLOAT
-  },
-  quantitySoldLastYear: {
-    type: DataTypes.INTEGER
-  },
-  quantitySoldThisYear: {
-    type: DataTypes.INTEGER
-  },
-  remainingStock: {
-    type: DataTypes.INTEGER
-  }
+  stickerNumber: { type: DataTypes.INTEGER, allowNull: false, unique: true },
+  releaseDate: { type: DataTypes.DATE },
+  wholesalePrice: { type: DataTypes.FLOAT },
+  retailPrice: { type: DataTypes.FLOAT },
+  quantitySoldLastYear: { type: DataTypes.INTEGER },
+  quantitySoldThisYear: { type: DataTypes.INTEGER },
+  remainingStock: { type: DataTypes.INTEGER }
 });
 
-// Определяем модель Компаний
-const Company = sequelize.define('Company', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  address: {
-    type: DataTypes.STRING
-  }
-});
 
-// Определяем связи между моделями
 Musician.belongsTo(Ensemble);
 Ensemble.hasMany(Musician);
 
@@ -84,7 +38,7 @@ Record.belongsToMany(Composition, { through: 'CompositionRecord' });
 Record.belongsTo(Company);
 Company.hasMany(Record);
 
-// Экспортируем модели
+
 module.exports = {
   Musician,
   Ensemble,
