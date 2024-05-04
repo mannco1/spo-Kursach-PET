cf48bd078ec6c88e8c7bbc5bd3724e775854542c
mannco1 <f.mannco@yandex.ru>
2024-05-04 13:17:18 +0300

    Update musiciansController.js

--- server/controllers/musiciansController.js
+++ server/controllers/musiciansController.js
@@ -12,7 +12,7 @@ class MusiciansController {
                 
             return res.json(musicians);
         } catch (err) {
-            // Если произошла ошибка при создании бренда, возвращаем ошибку API
+            
             return next(ApiError.internal('Ошибка при создании музыканта'));
         }
     }
@@ -23,7 +23,7 @@ class MusiciansController {
             
             return res.json(musicians);
         } catch (err) {
-            // Если произошла ошибка при получении списка брендов, возвращаем ошибку API
+            
             return next(ApiError.internal('Ошибка при получении списка музыкантов'));
         }
     }
@@ -47,16 +47,16 @@ class MusiciansController {
             // Находим бренд по ID
             const musicians = await Musicians.findByPk(id);
             if (!musicians) {
-                // Если бренд не найден, возвращаем ошибку API
+           
                 return next(ApiError.notFound('Музыкант не найден'));
             }
-            // Обновляем имя бренда
+           
             musicians.name = name;
-            // Сохраняем изменения в базе данных
+           
             await musicians.save();
             return res.json(musicians);
         } catch (err) {
-            // Если произошла ошибка при обновлении бренда, возвращаем ошибку API
+            
             return next(ApiError.internal('Ошибка при обновлении музыканта'));
         }
     }
@@ -64,11 +64,11 @@ class MusiciansController {
     async delete(req, res) {
         const { id } = req.params;
         try {
-            // Находим бренд по ID и удаляем его из базы данных
+            
             await Musicians.destroy({ where: { id } });
             return res.json({ message: 'музыкант успешно удален' });
         } catch (err) {
-            // Если произошла ошибка при удалении бренда, возвращаем ошибку API
+            
             return next(ApiError.internal('Ошибка при удалении музыканта'));
         }
     }
