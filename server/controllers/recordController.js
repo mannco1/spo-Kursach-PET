87daedc84832f2ac816aa9b3c49453ec90920a27
mannco1 <f.mannco@yandex.ru>
2024-05-04 18:44:01 +0300

    back_suroy
    бэкэнд post get работает для всех контролеров, делит частично /не тестировалось, PUT -серьезные проблемы

--- server/controllers/recordController.js
+++ server/controllers/recordController.js
@@ -1,16 +1,19 @@
 const { Record } = require('../models/models');
 const ApiError = require('../error/ApiError');
-
+const uuid = require('uuid')
+const path = require('path');
 
 class RecordController {
     async create(req, res, next) {
-
+ 
         try {
-            const { stickerNumber, releaseDate, wholesalePrice, retailPrice } = req.body;
-            
-            const record = await Record.create({ stickerNumber, releaseDate, wholesalePrice, retailPrice });
+            let { stickerNumber, releaseDate, wholesalePrice, retailPrice } = req.body;
+            const {img} = req.files
+            let fileName = uuid.v4() + ".jpg"
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+            const record = await Record.create({ stickerNumber, releaseDate, wholesalePrice, retailPrice,img: fileName });
 
-
+ 
 
 
             
@@ -45,18 +48,18 @@ class RecordController {
 
     async update(req, res, next) {
         const { id } = req.params;
-        const { stickerNumber, releaseDate, wholesalePrice, retailPrice } = req.body;
+        const { stickerNumber, releaseDate, wholesalePrice, retailPrice,  fileName } = req.body;
         try {
             let record = await Record.findByPk(id);
-            if (!record) {
+            if (!record) { 
                 return next(ApiError.notFound('Ансамбль не найден'));
             }
-
+  
             // Обновляем поля ансамбля
-            record = await Record.update({ stickerNumber, releaseDate, wholesalePrice, retailPrice });
+            record = await Record.update({ stickerNumber, releaseDate, wholesalePrice, retailPrice,img: fileName });
 
             // Возвращаем обновленный ансамбль
-            return res.json(record);
+            return res.json(record);  
         } catch (err) {
             return next(ApiError.internal('Ошибка при обновлении ансамбля'));
         }
--- server/models/models.js
+++ server/models/models.js
@@ -1,36 +1,42 @@
 const sequelize = require('../db')
 const {DataTypes} = require('sequelize')
 
-const Musician = sequelize.define('Musician', {
+const Musician = sequelize.define('musician', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
   name: { type: DataTypes.STRING, allowNull: false },
   instrument: { type: DataTypes.STRING }
 });
 
-const Ensemble = sequelize.define('Ensemble', {
+const Ensemble = sequelize.define('ensemble', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
   name: { type: DataTypes.STRING, allowNull: false },
   type: { type: DataTypes.STRING }
 });
 
 
-const Composition = sequelize.define('Composition', { 
+const Composition = sequelize.define('composition', { 
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
   title: { type: DataTypes.STRING, allowNull: false },
   composer: { type: DataTypes.STRING }
 });
 
 
-const Record = sequelize.define('Record', {
-  stickerNumber: { type: DataTypes.INTEGER, allowNull: false, unique: true },
+const Record = sequelize.define('record', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+  stickerNumber: { type: DataTypes.INTEGER, allowNull: false,  },
   releaseDate: { type: DataTypes.DATE, allowNull: false },
   wholesalePrice: { type: DataTypes.FLOAT, allowNull: false },
   retailPrice: { type: DataTypes.FLOAT, allowNull: false },
- 
+  img: {type: DataTypes.STRING, allowNull: false},
+  
 });
 
   
 
 
 
-const Company = sequelize.define('Company', {
+const Company = sequelize.define('company', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
   name: { type: DataTypes.STRING, allowNull: false },
   address: { type: DataTypes.STRING }
 });
@@ -39,11 +45,11 @@ const Company = sequelize.define('Company', {
 Musician.belongsTo(Ensemble);
 Ensemble.hasMany(Musician);
 
-// Composition.belongsToMany(Record, { through: 'CompositionRecord' });
-// Record.belongsToMany(Composition, { through: 'CompositionRecord' });
+Composition.belongsToMany(Record, { through: 'CompositionRecord' });
+Record.belongsToMany(Composition, { through: 'CompositionRecord' });
 
-// Record.belongsTo(Company);
-// Company.hasMany(Record);
+Record.belongsTo(Company);
+Company.hasMany(Record);
 
 
 module.exports = {
