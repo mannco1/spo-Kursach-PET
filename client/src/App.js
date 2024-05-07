7f33ec220bfe64142c108f8a631401717a917d26
mannco1 <f.mannco@yandex.ru>
2024-05-07 18:43:24 +0300

    music and Error404 pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import { Square } from '@mui/icons-material';
-import './pages/css/App.css'
-import Home from "./pages/Home.jsx";
-import Slider from "./pages/Slider.jsx";
-import SquareForms from './pages/SquareForms.jsx';
-import SquareFormsPaginator from './pages/SquareFormsPaginator.jsx';
-import MyPagination from './pages/MyPagination.jsx';
+
+import './components/css/App.css'
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage.jsx';
+import MusicPage from './pages/MusicPage.jsx';
+import Error404 from './pages/Error404.jsx';
 
 
 function App() {
   return (
-    <div className='glavniy'style={{ backgroundColor: '#000000', height: "100vw", }}>
-    <div className='container' style={{alignContent:'center'}}> 
-      <Home/>
-      <Slider/>
-      <SquareFormsPaginator/>
-
-      <MyPagination/>
+    <Router>
+    <div>
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+        <Route path="/music" element={<MusicPage/>} />
+        <Route  path="*" element={<Error404/>}/>
+      </Routes>
     </div>
-    </div>
+  </Router>
   );
 }
 
