e6e542d7f4e9a1619e6726207f778f10bc8a3cbb
mannco1 <f.mannco@yandex.ru>
2024-05-10 22:55:01 +0300

    fix

--- client/src/components/SquareFormsPaginator.jsx
+++ client/src/components/SquareFormsPaginator.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import SquareForms from './SquareForms'
 import { Button } from '@mui/material';
@@ -17,24 +16,4 @@ const SquareFormsPaginator = () => {
     );
 };
 
-=======
-import React from 'react';
-import SquareForms from './SquareForms'
-import { Button } from '@mui/material';
-import './css/Home.css'
-
-const SquareFormsPaginator = () => {
-    
-
-    return (
-        <div >
-            <SquareForms firstP={0} secondP={8} />
-            <div >
-            <Button variant="outlined" color="error" ><span ><a style={{fontSize:'1.5rem',color:'white',fontFamily:'JetBrains Mono',}} href='/music'>SHOW MORE</a></span></Button>
-       </div>
-        </div>
-    );
-};
-
->>>>>>> 91f02e22980dd7dc9338a207f427d607117b1d1e
 export default SquareFormsPaginator;
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Home from "../components/Home.jsx";
 import Slider from "../components/Slider.jsx";
@@ -21,27 +20,3 @@ const MainPage = () => {
 
 export default MainPage;
 
-=======
-import React from 'react';
-import Home from "../components/Home.jsx";
-import Slider from "../components/Slider.jsx";
-import '../components/css/App.css'
-import SquareFormsPaginator from '../components/SquareFormsPaginator.jsx';
-import MyPagination from '../components/MyPagination.jsx';
-
-const MainPage = () => {
-    return (
-        <div className='glavniy'style={{ backgroundColor: '#000000', height: "100vw", }}>
-        <div className='container' style={{alignContent:'center'}}> 
-        <Home/>
-          <Slider/>
-          <SquareFormsPaginator/>
-          
-        </div>
-        </div>
-    );
-};
-
-export default MainPage;
-
->>>>>>> 91f02e22980dd7dc9338a207f427d607117b1d1e
