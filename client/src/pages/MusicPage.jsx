086372cad86f2de6feb21bc4cbc864497374d7d7
mannco1 <f.mannco@yandex.ru>
2024-05-10 22:00:05 +0300

    fixgit

--- client/src/pages/MusicPage.jsx
+++ client/src/pages/MusicPage.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import MyPagination from '../components/MyPagination';
 import Home from '../components/Home';
@@ -15,22 +15,4 @@ const MusicPage = () => {
     );
 };
 
-=======
-import React from 'react';
-import MyPagination from '../components/MyPagination';
-import Home from '../components/Home';
-
-const MusicPage = () => {
-    return (
-        <div className='glavniy'style={{ backgroundColor: '#000000', height: "110%", }}>
-        <div className='container' style={{alignContent:'center'}}> 
-        <Home/>
-         <MyPagination/>
-          
-        </div>
-        </div>
-    );
-};
-
->>>>>>> 91f02e22980dd7dc9338a207f427d607117b1d1e
 export default MusicPage;
